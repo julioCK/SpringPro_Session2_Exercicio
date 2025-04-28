745d3c42b14010baa90d698ac598b2284b30df4a
julioCK <bocumaa@gmail.com>
2025-04-28 11:43:54 -0300

    first commit

---
+++ src/main/java/com/juliock/SpringPro_Session2_Exercicio/SpringProSession2ExercicioApplication.java
@@ -0,0 +1,13 @@
+package com.juliock.SpringPro_Session2_Exercicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProSession2ExercicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProSession2ExercicioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juliock/SpringPro_Session2_Exercicio/SpringProSession2ExercicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juliock.SpringPro_Session2_Exercicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProSession2ExercicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
